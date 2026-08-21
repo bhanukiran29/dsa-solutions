dca01de7cbeaa8e3c81e45311908002d31f956a6
Madisetty Bhanu Kiran <bhanu.madisetty@gmail.com>
2026-08-21 23:48:54 +0530

    Time: 527 ms (41.72%), Space: 243.5 MB (16.14%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,73 @@
+class Solution {
+private:
+    void merge(vector<int>& nums, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+
+        while(left <= mid && right <= high) {
+            if(nums[left] <= nums[right]) {
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else {
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid) {
+            temp.push_back(nums[left]);
+            left++;
+        }
+
+        while(right <= high) {
+            temp.push_back(nums[right]);
+            right++;
+        }
+
+        for(int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+    }
+
+    int countPairs(vector<int>& nums, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+
+        for(int i = low; i <= mid; i++) {
+            while(right <= high &&
+                  (long long)nums[i] > 2LL * nums[right]) {
+                right++;
+            }
+
+            cnt += right - (mid + 1);
+        }
+
+        return cnt;
+    }
+
+    int mS(vector<int>& nums, int low, int high) {
+        if(low >= high) return 0;
+
+        int mid = (low + high) / 2;
+
+        int cnt = 0;
+
+        cnt += mS(nums, low, mid);
+        cnt += mS(nums, mid + 1, high);
+
+        cnt += countPairs(nums, low, mid, high);
+
+        merge(nums, low, mid, high);
+
+        return cnt;
+    }
+
+public:
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+
+        return mS(nums, 0, n - 1);
+    }
+};
